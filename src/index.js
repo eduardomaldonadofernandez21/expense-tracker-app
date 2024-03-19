ace99ad4bb40c840e798d835d0fa1fd9975d0ffb
eduardomaldonadofernandez21 <edumaldonadofdez21@gmail.com>
2024-03-19 10:23:33 +0000

    creation with states context and several components

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { theme } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import GlobalState from './context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <GlobalState>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </GlobalState>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
